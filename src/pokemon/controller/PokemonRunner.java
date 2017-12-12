f4d04f6803ffba867743e8800cca55536eb13d3d
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2017-12-12 13:01:43 -0700

    I stared working on the Jframe

--- src/pokemon/controller/PokemonRunner.java
+++ src/pokemon/controller/PokemonRunner.java
@@ -2,5 +2,10 @@ package pokemon.controller;
 
 public class PokemonRunner
 {
+	public static void main(String args[])
+	{
+		PokemonController app = new PokemonController();
+		app.start();
+	}
 
 }
