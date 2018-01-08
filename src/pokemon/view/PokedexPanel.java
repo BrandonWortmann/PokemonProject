ddd547d12509834d0e1af740b4c0b9cb030087d6
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-01-08 12:55:35 -0700

    I addeda lot to the pokedexPanel

---
+++ src/pokemon/view/PokedexPanel.java
@@ -0,0 +1,101 @@
+package pokemon.view;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+import pokemon.controller.PokemonController;
+
+public class PokedexPanel extends JPanel
+{
+	
+	private PokemonController appController;
+	private SpringLayout appLayout;
+	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evolvableBox;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
+	
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokedexDropdown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
+
+	
+	
+	
+	
+	
+	public PokedexPanel(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		appLayout = new SpringLayout();
+		
+		evolvableBox = new JCheckBox();
+		nameField = new JTextField("name");
+		numberField = new JTextField("##");
+		attackField = new JTextField("ap");
+		healthField = new JTextField("hp");
+		modifierField = new JTextField("mod");
+		
+		//iconLabel = new JLabel()
+		
+		nameLabel = new JLabel("name");
+		evolvableLabel = new JLabel("evolvable");
+		numberLabel = new JLabel("number");
+		attackLabel = new JLabel("attack");
+		healthLabel = new JLabel("health");
+		modifierLabel = new JLabel("modifier");
+		pokedexDropdown = new JComboBox();
+		clearButton = new JButton("clear");
+		saveButton = new JButton("save");
+		
+		descriptionArea = new JTextArea(5, 10);
+		typeArea = new JTextArea(4, 15);
+		
+		firstType = new JPanel();
+		secondType = new JPanel();
+		thirdType = new JPanel();
+		fourthType = new JPanel();
+		
+	}
+	
+	
+	
+	
+	private void setupComboBox()
+	{
+		DefaultComboBoxModel pokemonModel = new DefaultComboBoxModel(appController.convertPokedex());
+		pokedexDropdown.setModel(pokemonModel);
+	}
+	
+	private void setupTypePanels()
+	{
+		firstType.setSize(50, 50);
+		secondType.setSize(50, 50);
+		thirdType.setSize(50, 50);
+		fourthType.setSize(50, 50);
+	}
+
+}
